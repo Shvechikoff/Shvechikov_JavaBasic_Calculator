5879fbbab89724360502b5a65d67e41c3c15bf07
Illia <illia.shvechikov@gmail.com>
2022-09-15 14:05:46 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-//сообщение - ввод данных - сообщение - ввод данных - сообщение - результат - вывод
+//сообщение - ввод данных - сообщение - ввод данных - результат - вывод
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
